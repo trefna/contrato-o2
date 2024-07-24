bbdaffd36bafefc2f53f941a19433a0bc2d2a635
trefna <smithanfert@gmail.com>
2024-07-24 15:38:06 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,15 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const contractRoutes = require('./routes/contractRoutes');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use('/api/contracts', contractRoutes);
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
