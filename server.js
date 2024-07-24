220ba67c4a5a4610aee017a4bd8279a54df064a3
trefna <smithanfert@gmail.com>
2024-07-24 20:47:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const contractRoutes = require('./routes/contractRoutes');
+const contractRoutes = require('./server/routes/contractRoutes'); // Ajusta la ruta a ./server/routes/contractRoutes
 
 const app = express();
 app.use(cors());
