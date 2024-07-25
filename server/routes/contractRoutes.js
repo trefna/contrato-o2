9cf42200d71d98f4d347537265458858b288280d
trefna <smithanfert@gmail.com>
2024-07-25 17:06:07 +0200

    Update contractRoutes.js

--- server/routes/contractRoutes.js
+++ server/routes/contractRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { generateContract, signContract, getContract } = require('../controllers/contractController'); // Asegúrate de que la ruta es correcta
+const { generateContract, signContract, getContract } = require('../controllers/contractController');
 
 router.post('/generate', generateContract);
 router.post('/sign', signContract);
-router.get('/:id', getContract);
+router.get('/:contractId', getContract);
 
 module.exports = router;
