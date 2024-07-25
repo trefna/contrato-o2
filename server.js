6695144abd6b612d9e2e58a35d8f1ce2d86eb89d
trefna <smithanfert@gmail.com>
2024-07-25 20:21:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const contractRoutes = require('./server/routes/contractRoutes');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(bodyParser.json());
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/admin-frontend/build')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/contracts', contractRoutes);
 
-// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/admin-frontend/build')));
+
+// Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/admin-frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname + '/client/admin-frontend/build/index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
