a64ff60f44a1875d27484eca91c5f6f158722ed3
trefna <smithanfert@gmail.com>
2024-07-25 17:13:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const contractRoutes = require('./routes/contractRoutes');
+const contractRoutes = require('./server/routes/contractRoutes');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/admin-frontend/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/contracts', contractRoutes);
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/admin-frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
